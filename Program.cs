aff19fc5a314efa49b7814b8a0388826dcc123b3
Aléxia <alexia.karoline@ufv.br>
2026-01-12 20:26:28 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+		
+		Console.WriteLine("Tabuada de 1 a 10:");
+
+        for (int i = 1; i<=10;i++)
+        {
+            Console.WriteLine();
+            for(int j = 1; j <= 10; j++)
+            {
+                Console.WriteLine($"{i} x {j} = {i*j}");
+            }
+        }
+
+    }
+}
